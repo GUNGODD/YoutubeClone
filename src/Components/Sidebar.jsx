0c16284d2ec0e884b6613a639b1fc6c1d2981e86
GUNGODD <chauhanadityac01@gmail.com>
2024-02-17 14:31:35 +0530

    added files

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,17 +3,9 @@ export default function Sidebar() {
   return (
         <>
               <div>
-<<<<<<< HEAD
-                    <div className="border border-gray-800 w-[15%]">
-                          <div className="flex">
-                                  <FiHome  size={"24px"}/>
-                          <p>Home</p>
-                        </div>
-=======
                     <div>
                           <FiHome />
                           title
->>>>>>> 43c4cbde815558406fbe9eb54445067149c04385
                     </div>
         </div>
         
