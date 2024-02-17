d9a853281be1a462bdef64991e91d52dab220644
GUNGODD <chauhanadityac01@gmail.com>
2024-02-17 12:31:25 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { CgMenu } from "react-icons/cg"; 
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+
+
+const Navbar = () => {
+      
+      return (
+            <>
+                  <div className="">
+                        
+                       
+                                 
+                        <CgMenu />
+                        <img src="/src/assets/logo.png" alt="" />
+                        <div></div>
+                        <div></div>
+                  </div>
+                  
+            </>
+      );
+}
+
+export default Navbar;
