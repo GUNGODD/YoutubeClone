c5c75f6cd6a98f3c75fcf36eeafa46c423919a7c
GUNGODD <chauhanadityac01@gmail.com>
2024-02-17 12:53:23 +0530

    navbar div added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,14 +11,19 @@ const Navbar = () => {
             <>
                   <div>
                         
-                        <div className="">
+                        <div className="flex items-center">
                               
                               <GiHamburgerMenu />
-                              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1920px-YouTube_Logo_2017.svg.png" alt="" />
-                              
+                              <img width={ 115} src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1920px-YouTube_Logo_2017.svg.png" alt="" />
+
                         </div>
-                        <div></div>
-                        <div></div>
+                        <div>  {/* input section */}
+                              <input type="text" className="border border-gray-200"/>
+                        </div>
+                        
+                        <div>
+                              <button>Search</button>
+                        </div>
                  </div>
                   
             </>
