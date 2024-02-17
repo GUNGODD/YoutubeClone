0c226478a67cc58761bae60a2cde1eada0841e1d
GUNGODD <chauhanadityac01@gmail.com>
2024-02-17 12:48:55 +0530

    navbar #1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,25 @@
-import { CgMenu } from "react-icons/cg"; 
 
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
 
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
       
       return (
             <>
-                  <div className="">
+                  <div>
                         
-                       
-                                 
-                        <CgMenu />
-                        <img src="/src/assets/logo.png" alt="" />
+                        <div className="">
+                              
+                              <GiHamburgerMenu />
+                              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1920px-YouTube_Logo_2017.svg.png" alt="" />
+                              
+                        </div>
                         <div></div>
                         <div></div>
-                  </div>
+                 </div>
                   
             </>
       );
